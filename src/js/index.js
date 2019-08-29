bfbdfa3b6ee75f1e7ea4c1923e1b68534939c0da
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-08-29 16:48:40 -0500

    changes 5

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,22 @@
-import apiCall from "./services/api/apiCall";
-const client_id = process.env.GITHUB_CLIENT_ID;
-const client_secret = process.env.GITHUB_CLIENT_SECRET;
-const route = "/nliebhartbellevue";
+// Init Github request
+const github = new Github();
+const user = "nliebhartbellevue";
 
-class Index {
-  constructor() {
-    this.$profile = document.querySelector("#profile");
-  }
-
-  getProfile() {
-    apiCall(route, client_id, client_secret)
-      .then(response => {
-        if (response.profile.message === "Not Found") {
-          console.log("Profile Not Found");
-        } else {
-          showProfile(response.profile);
-        }
-      })
-      .catch(() => {
-        console.log("Error!");
-      });
-  }
-
-  showProfile(user) {
-    this.$profile.innerHTML = `
-        <div>
-            <img src="${user.avatar_url}">
-        </div>
-      `;
-  }
-}
+const showProfile = user => {
+  const $profile = document.querySelector("#profile");
+  $profile.innerHTML = `
+    <div>
+      <img src="${user.avatar_url}">
+    </div>
+  `;
+};
 
 window.addEventListener("load", () => {
-  new Index();
+  github.getUser(user).then(data => {
+    if (data.profile.message === "Not Found") {
+      console.log("Profile Not Found!");
+    } else {
+      showProfile(data.profile);
+    }
+  });
 });
