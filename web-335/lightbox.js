c9dadf844ec10607a723d2a3befea7dff67e611a
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-11-01 16:52:04 -0500

    lighbox work

---
+++ web-335/lightbox.js
@@ -0,0 +1,10 @@
+const openWeek1Modal = () => {
+  document.getElementById('week1-modal').style.display = 'block';
+};
+
+const closeWeek1Modal = () => {
+  document.getElementById('week1-modal').style.display = 'none';
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
